616f8b2ccd79929f8b5143322ccdd22bd074e1e0
Gustavo <gusta.paz@hotmail.com>
2026-08-16 21:31:40 -0300

    Atualiza teste de widget com Firebase fake para inicializacao

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,44 @@
+import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rede_social/main.dart';
 
+class FakeFirebaseAppPlatform extends FirebaseAppPlatform {
+  FakeFirebaseAppPlatform()
+      : super(
+          '[DEFAULT]',
+          const FirebaseOptions(
+            apiKey: 'fake-api-key',
+            appId: 'fake-app-id',
+            messagingSenderId: 'fake-sender-id',
+            projectId: 'fake-project-id',
+          ),
+        );
+}
+
+class FakeFirebasePlatform extends FirebasePlatform {
+  final _app = FakeFirebaseAppPlatform();
+
+  @override
+  FirebaseAppPlatform app([String name = defaultFirebaseAppName]) => _app;
+
+  @override
+  List<FirebaseAppPlatform> get apps => [_app];
+
+  @override
+  Future<FirebaseAppPlatform> initializeApp({
+    String? name,
+    FirebaseOptions? options,
+  }) async {
+    return _app;
+  }
+}
+
 void main() {
+  setUpAll(() {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    FirebasePlatform.instance = FakeFirebasePlatform();
+  });
+
   testWidgets('App inicializa sem erros', (WidgetTester tester) async {
     await tester.pumpWidget(const MeuApp());
     await tester.pump();
